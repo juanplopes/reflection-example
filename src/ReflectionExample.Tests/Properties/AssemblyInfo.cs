7295175608e6b6e91996959c606e08f46fe6f540
Juan Lopes <juanplopes@gmail.com>
2011-01-10 10:32:46 -0200

    initial structure

---
+++ src/ReflectionExample.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReflectionExample.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("237610ce-8faa-41a9-96a4-ab9471e07f16")]
---
+++ src/ReflectionExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReflectionExample")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("c06bd40f-3104-4310-abbe-df8c08416c59")]
