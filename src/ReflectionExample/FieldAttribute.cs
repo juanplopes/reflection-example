50c0b31ed435e8e51b0bb79a5af63833387ea2d1
Juan Lopes <juanplopes@gmail.com>
2011-01-10 13:26:00 -0200

    date field

--- src/ReflectionExample/FieldAttribute.cs
+++ src/ReflectionExample/FieldAttribute.cs
@@ -21,7 +21,7 @@ namespace ReflectionExample
 
         public virtual void WriteTo(StringBuilder builder, object value)
         {
-            var stringValue = Convert.ToString(value);
+            var stringValue = ConvertToString(value);
 
             builder.Length = Math.Max(builder.Length, end);
 
@@ -29,12 +29,22 @@ namespace ReflectionExample
             WriteSpaces(builder, stringValue);
         }
 
+        protected virtual string ConvertToString(object value)
+        {
+            return Convert.ToString(value, CultureInfo.InvariantCulture);
+        }
+
         public virtual object ReadFrom(string line, Type type)
         {
             var stringValue = line.Substring(start, Math.Min(line.Length - start, Size));
             if (Trim) stringValue = stringValue.Trim('\0', ' ');
-            
-            return Convert.ChangeType(stringValue, type);
+
+            return ConvertFromString(type, stringValue);
+        }
+
+        protected virtual object ConvertFromString(Type type, string stringValue)
+        {
+            return Convert.ChangeType(stringValue, type, CultureInfo.InvariantCulture);
         }
 
         private void WriteString(StringBuilder builder, string stringValue)
