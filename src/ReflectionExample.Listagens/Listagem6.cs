4df321594710992c9ac083fed53ad79b2a2c8209
Juan Lopes <juanplopes@gmail.com>
2011-01-12 15:30:30 -0200

    modificacoes para melhorar o exemplo

---
+++ src/ReflectionExample.Listagens/Listagem6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace ReflectionExample.Listagens
+{
+    public class Listagem6
+    {
+        public static void Test()
+        {
+            var propA = typeof(Foo).GetProperty("PropertyA");
+
+            var foo = Expression.Parameter(typeof(Foo));
+            var value = Expression.Parameter(typeof(int));
+
+            var assign = Expression.Assign(Expression.Property(foo, propA), value);
+            var lambda = Expression.Lambda<Action<Foo, int>>(assign, foo, value).Compile();
+
+            var instance = new Foo();
+
+            lambda(instance, 42);
+            Console.WriteLine(instance.PropertyA);
+        }
+    }
+}
